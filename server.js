a86fbbeb87be17dd7b137bd18691ebcca6e98c2c
RACKYWEB-web <edwardzethan792@gmail.com>
2026-04-29 01:48:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
+app.use(express.json());
+
+app.post("/api/form", (req, res) => {
+  console.log(req.body); // shows data you receive
+
+  res.json({
+    message: "Data received successfully",
+    data: req.body
+  });
 });
 
 app.listen(process.env.PORT || 3000);
