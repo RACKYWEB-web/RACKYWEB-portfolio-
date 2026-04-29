b0ac42ae263a2cc3461843b605ab88b40ad694e1
RACKYWEB-web <edwardzethan792@gmail.com>
2026-04-29 02:16:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+// homepage route (FIX for Cannot GET /)
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
+
+// form route
 app.post("/api/form", (req, res) => {
-  console.log(req.body); // shows data you receive
+  console.log(req.body);
 
   res.json({
     message: "Data received successfully",
